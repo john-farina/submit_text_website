a94ac4832b912d2d0ba9ef0cbfea09579e624f60
onefootfarina <clichecontent@gmail.com>
2022-08-21 17:44:34 -0400

    styled the seperate pages

--- app/helpers/texts_helper.rb
+++ app/helpers/texts_helper.rb
@@ -9,7 +9,23 @@ module TextsHelper
         text_name = text_name.titleize
         text_name + " :"
       else
+        text_name = text_name.upcase
         text_name + " :"
+      end
+    end
+  end
+
+  def auto_page_name(text_name = ' ')
+    base_name = "?"
+    if text_name.empty?
+      base_name
+    else
+      if text_name.length > 2
+        text_name = text_name.titleize
+        text_name + ""
+      else
+        text_name = text_name.upcase
+        text_name + ""
       end
     end
   end
